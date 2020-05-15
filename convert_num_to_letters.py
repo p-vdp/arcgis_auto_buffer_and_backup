385c48a80d74bb08975c6c708a03b6d533b57315
tanneryould <48941951+tanneryould@users.noreply.github.com>
2020-05-15 14:43:31 -0700

    standalone script to convert nums to letters
    For example, 29 -> AC

---
+++ convert_num_to_letters.py
@@ -0,0 +1,37 @@
+alphabet = {
+    1: 'A',
+    2: 'B',
+    3: 'C',
+    4: 'D',
+    5: "E",
+    6: "F",
+    7: "G",
+    8: "H",
+    9: "I",
+    10: "J",
+    11: "K",
+    12: "L",
+    13: "M",
+    14: "N",
+    15: "O",
+    16: "P",
+    17: "Q",
+    18: "R",
+    19: "S",
+    20: "T",
+    21: "U",
+    22: "V",
+    23: "W",
+    24: "X",
+    25: "Y",
+    26: "Z"
+}
+
+def convert_num_to_letters(num):
+    ret = ''
+
+    while num > 0:
+        num, remainder = divmod(num, 26)
+        ret = alphabet[int(remainder)] + ret
+    
+    return ret
